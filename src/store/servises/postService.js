e328347156eba45519220bb172dec9f09fa724c7
Ira <banatovskaya@gmail.com>
2022-10-17 13:32:25 +0300

    change baseUrl for Heroky

--- src/store/servises/postService.js
+++ src/store/servises/postService.js
@@ -3,7 +3,9 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const postAPI = createApi({
     reducerPath: 'postAPI',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3001'
+        // baseUrl: 'http://localhost:3001'
+        baseUrl: 'https://shopapptest.herokuapp.com/api'
+
     }),
     endpoints: builder => ({
         getData: builder.query({  
