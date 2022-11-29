a130f6b1e0967e96f9595a6650967a1772531fab
Ira <banatovskaya@gmail.com>
2022-11-29 15:51:38 +0200

    add carousel

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,13 +1,48 @@
 import Menu from "../menu/menu";
 import Container from '@mui/material/Container';
+import React from 'react';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
+import { postAPI } from "../../store/services/postService";
+import imgErr from '../../assets/img.jpg';
 
 const HomePage = () => {
+    const {
+        data: products = [] ,
+        isfetshing,
+        isLoading,
+        isError,
+        error
+    } = postAPI.useGetDataQuery('productList');
+
+    if (isLoading || isfetshing) {
+        return <h1>loading...</h1>
+    } else if (isError) {
+        return <h1>Error {error.status} </h1>
+    };
+
+    const getImages = (idesForCarousel, products) => {
+        let arr=[];
+        idesForCarousel.forEach(el => {
+            const itemForSlider = products.find((item) => (+item.id === el))
+            if (itemForSlider) {arr.push(itemForSlider)}  
+        });
+        if (arr.length <= 0){
+            return [{src: imgErr}]
+        } else {
+            return arr.map((item) => ({src: `../images/${item.image}`})) 
+        };
+    };
+
+    const idImgForCarousel = [1,3,127];
+    const images = getImages(idImgForCarousel, products);
+   
     return (
         <>
         <Menu/>
         <Container maxWidth="sm" sx={{mt:13 }} >
-            <h1>ще не дороблено перейдіть будь ласка на вкладку product Category там можна перейти на сторінку товару і додати товар у кошик, кошик дозволяє коригувати кількість товару від 1 до 99 </h1>
-            <h2>тут буде слайдер та останні перегляди</h2>
+            <Carousel images={(images)} />
+            <h2>нижче будуть останні перегляди</h2>
         </Container>
         </>
     )
